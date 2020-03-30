d1fe4af5c7abb89db95352cd3d15ffdc84d8bd11
Karan Shah <62107454+karanshah0206@users.noreply.github.com>
2020-03-30 13:57:30 +0530

    Improved Comment Clarity + Beautified Code
    Some of the comments weren't clear while others weren't necessary. I also made the code a bit more organized by changing the spacing conditions to make it look neater.

--- Mega_TFT_DataLogger_RTClock/Peltier.ino
+++ Mega_TFT_DataLogger_RTClock/Peltier.ino
@@ -1,115 +1,93 @@
-/**
- * Initialize peltier Control library and define pins
- * Set initial intensity and direction to 0
- */
+/*
+Initialize peltier Control library and define pins
+Set initial intensity and direction to 0
+*/
 void initPeltier() {
-
   PC.begin();
 }
 
-/**
- * peltier need to cool or heat, 
- * depending on the water and target temperatur
- * if not set differently, tempTarget is 24C
- */
+/*
+Peltier needs to cool or heat
+Depending on the water and target temperatur
+If not set differently, tempTarget is 24C
+*/
 void peltierControl() {
-
   int tempDifference = (inputData.tempWater - inputData.tempTarget) * 10;
-  // Example: tempDifference < 0: water is to cold (20C (tempWater) - 24C (tempTarget) = -4 (tempDifference))
+    switch(PC.peltierState) {
+      case PeltierStopped:
+        if(tempDifference <= (-hysteresis - epsilon)) {
+          PC.heat(calculatePwm(tempDifference, PC.peltierState));
+        } 
+        else if (tempDifference >= (hysteresis + epsilon)) {
+          PC.cool(calculatePwm(tempDifference, PC.peltierState));
+        }
+        else {
+          PC.stop();
+        }    
+      break;
+      
+      case PeltierHeating:
+        if ((tempDifference > -hysteresis) && (tempDifference <= 0)) {
+          PC.stop();
+        }
+        else {
+          long val = calculatePwm(tempDifference, PC.peltierState);
+          if (val != 0) {
+            PC.heat(val);
+          } 
+          else {
+            PC.stop();
+          }
+        }
+      break;
 
-  switch(PC.peltierState)
-  {
-
-  case PeltierStopped:
-    if(tempDifference <= (-hysteresis - epsilon)) {
-      PC.heat(calculatePwm(tempDifference, PC.peltierState));
-    } 
-    else if (tempDifference >= (hysteresis + epsilon)) {
-      PC.cool(calculatePwm(tempDifference, PC.peltierState));
+      case PeltierCooling:
+        if ((tempDifference < hysteresis) && (tempDifference > 0)) {
+          PC.stop();
+        }
+        else {
+          long val = calculatePwm(tempDifference, PC.peltierState);
+          if (val != 0) {
+            PC.cool(val);
+          } 
+          else {
+            PC.stop();
+          }
+        }
+      break;
     }
-    else {
-      PC.stop();
-    }    
-    break;
-
-  case PeltierHeating:
-    if ((tempDifference > -hysteresis) && (tempDifference <= 0)) {
-      PC.stop();
-    }
-    else {
-      long val = calculatePwm(tempDifference, PC.peltierState);
-      if (val != 0) {
-        PC.heat(val);
-      } 
-      else {
-        PC.stop();
-      }
-    }
-
-    break;
-
-  case PeltierCooling:
-    if ((tempDifference < hysteresis) && (tempDifference > 0)) {
-      PC.stop();
-    }
-    else {
-      long val = calculatePwm(tempDifference, PC.peltierState);
-      if (val != 0) {
-        PC.cool(val);
-      } 
-      else {
-        PC.stop();
-      }
-    }
-    break;
-  }
 }
 
-/**
- * Calculate neded PWM value
- */
+/*
+Calculate required PWM value
+*/
 int calculatePwm(int tempDiff, PeltierState state) {
-  // set intensity of peltier depending on the temperature difference
-
-    int value;
-    //int value = map(abs(tempDiff), hysteresis, 3, 0, 255);
-
+  int value;
   if (abs(tempDiff) > hysteresis ) {
     value = 255;
   } 
   else {
     value = 0;
   }
-
   if (value > 255) {
     value = 255;
   }
   if (value < 0) {
     value = 0;
   }
-
-  switch (state)
-  {
-  case PeltierStopped:
-    peltierPwmValue = 0;
-    value = 0;
-    break;
-
-  case PeltierCooling:
-    peltierPwmValue = -value;
-    break;
-
-  case PeltierHeating:
-    peltierPwmValue = value;
-    break;
+  switch (state) {
+    case PeltierStopped:
+      peltierPwmValue = 0;
+      value = 0;
+      break;
+      
+    case PeltierCooling:
+      peltierPwmValue = -value;
+      break;
+      
+    case PeltierHeating:
+      peltierPwmValue = value;
+      break;
   }
   return value;
 }
-
-
-
-
-
-
-
-
